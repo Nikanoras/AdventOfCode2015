fc326069c4b43de440659be34626b173269a5f45
Nikanoras Zavadskij <nz@intersurgical.lt>
2020-04-27 15:57:09 +0300

    first commit

---
+++ Src/Day1NotQuiteLisp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Day1NotQuiteLisp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] directions = File.ReadAllText("input.txt").ToCharArray();
+
+            var floorNavigator = new SantaFloorNavigator(directions);
+
+            int floor = floorNavigator.GetFinalDestination();
+
+            Console.WriteLine($"The santa finally reached his destination at floor: {floor}!");
+
+            int firstBasementVisitDirectionPosition = floorNavigator.GetPositionOfFirstDirectionForSantaToVisitBasement();
+
+            Console.WriteLine($"Very interesting fact: The santa visited basement for the first time on direction number: {firstBasementVisitDirectionPosition}!");
+        }
+    }
+}
---
+++ Src/Day1NotQuiteLisp/SantaFloorNavigator.cs
@@ -0,0 +1,59 @@
+﻿namespace Day1NotQuiteLisp
+{
+    internal sealed class SantaFloorNavigator
+    {
+        private readonly char[] _directions;
+
+        internal SantaFloorNavigator(char[] directions)
+        {
+            _directions = directions;
+        }
+
+        internal int GetFinalDestination()
+        {
+            var currentSantasFloor = 0;
+            foreach (char direction in _directions)
+            {
+                currentSantasFloor += GetNextSantasDirection(direction);
+            }
+
+            return currentSantasFloor;
+        }
+
+        internal int GetPositionOfFirstDirectionForSantaToVisitBasement()
+        {
+            var currentSantasFloor = 0;
+            for (int i = 0; i < _directions.Length; i++)
+            {
+                currentSantasFloor += GetNextSantasDirection(_directions[i]);
+                if (IsBasement(currentSantasFloor))
+                    return i + 1;
+            }
+            return 0;
+
+            static bool IsBasement(int currentFloor)
+            {
+                return currentFloor == -1;
+            }
+        }
+
+        private static int GetNextSantasDirection(char direction)
+        {
+            if (IsUpstairs(direction))
+                return 1;
+            else if (IsDownstairs(direction))
+                return -1;
+            return 0;
+
+            static bool IsUpstairs(char direction)
+            {
+                return direction == '(';
+            }
+
+            static bool IsDownstairs(char direction)
+            {
+                return direction == ')';
+            }
+        }
+    }
+}
